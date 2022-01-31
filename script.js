0980a26b95f21d7863ccd8210d2580ce4c5716a7
Amanda Genovese <genovese.amanda@gmail.com>
2022-01-31 11:57:00 -0500

    modified css & cleaned up functions

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 const container = document.querySelector('#container');
 let cell;
-let target; 
-
-// makes cells(divs)
+let target;
 
 function makeCell (numCell) {
     for (let c = 0; c < numCell; c++) {
@@ -11,9 +9,6 @@ function makeCell (numCell) {
         container.appendChild(cell);
     }
 }
-
-
-//cells within each row. Example makeGrid(4,3), loop would run 4 times makeing  3 cells/colums per row each loop
 
 function makeGrid (numRow, numCol) {
     container.style.setProperty('--numRows', numRow); 
@@ -24,37 +19,31 @@ function makeGrid (numRow, numCol) {
 }
 
 function promptGrid() {
-   let number = prompt("Choose grid size between 5-64!", 16); 
+   let number = prompt("How big do you want your grid? Pick a number up to 64", 16); 
     if (number >= 5 && number <= 64) {
         makeGrid (number,number);
-    }else {
+    }
+    else {
         do {
-            number = prompt("Invalid size! Try Again! Make sure your value is between 5-64!");
+            number = prompt("No! Needs to be between 5 and 64!!!");
         }
         while(number < 5 || number > 64);
         makeGrid (number,number);
     }
 }
 
-
-//change color when mouseover 
-
-function changeColor (target) {
+function onHover (target) {
     target.style.backgroundColor = 'black';
 }
 
 container.addEventListener("mouseover", function (e) {
     target = e.target;
-
     if (target.matches("div.cell")) {
-        changeColor(target);
+        onHover(target);
     }
 });
 
-
-//reset button
-
-const reset = document.querySelector('.resetBtn');
+const reset = document.querySelector('.resetButton');
 reset.addEventListener('click', function(){
      window.location.reload();
 });
